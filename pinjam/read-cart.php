ffb9777d04ad378938ea6c2a258fd7bd4a7510a7
alxjesse <alxjesse@github.com>
2018-10-02 09:27:19 +0700

    read new

--- pinjam/read-cart.php
+++ pinjam/read-cart.php
@@ -12,11 +12,52 @@ session_start();
 </head>
 <body>
 <div class="container" style=" padding: 8%; padding-top: 2%; margin: 10%; border:2px solid #12a8bb;">
-	<h2 style="text-align: center; margin-bottom: 10%;">YOUR CART </h2>
+	<h2 style="text-align: center; margin-bottom: 5%;">DAFTAR BUKU </h2>
 		<div class="col-md-12">
 			<div class="row" style="text-align: center;">
 			
 
+	 <div class="col-md-12 col-sm-12">
+            <div class="table-responsive" style="margin-top: 2%;">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                  	 <th scope="col">ID BUKU</th>
+                    <th scope="col">JUDUL BUKU</th>
+                    <th scope="col">HAPUS BUKU</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php	
+                  
+                  $link = mysqli_connect("localhost", "root", "admin", "perpustakaan");
+		$query ="SELECT id, judul FROM buku";
+		$result = mysqli_query( $link, $query );
+		while ($baris = mysqli_fetch_array($result)) {
+			$hasil['id'] = $baris['id'];
+			$hasil['judul'] = $baris['judul'];
+
+			?>
+			 <tr >
+                    <td class="text-center"><?php echo $hasil['id'] ?></td>
+                    <td><?php echo $hasil['judul']?></td>
+                    <td><a href="<?php  echo "add-cart.php?id=$hasil[id]";?>">Tambah</a></td>
+                   
+                  </tr>
+
+
+
+			<?php
+					}
+			?>
+			
+                  
+                </tbody>
+              </table>
+
+            </div>
+
+<h2 style="text-align: center; margin: 5% 0;">CART ANDA </h2>
 				 <div class="col-md-12 col-sm-12">
             <div class="table-responsive" style="margin-top: 2%;">
               <table class="table table-striped">
